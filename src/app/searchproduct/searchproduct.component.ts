ce820cedfbc46a08801c28bde1e8406e4e63495f
fasilmsageer <fasilmsageer@gmail.com>
2022-12-07 20:02:03 +0530

    adminlogin

---
+++ src/app/searchproduct/searchproduct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchproduct',
+  templateUrl: './searchproduct.component.html',
+  styleUrls: ['./searchproduct.component.css']
+})
+export class SearchproductComponent {
+
+}
